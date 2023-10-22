4fcc5073052a594701dca97819d9804a2dc1126e
Ruben870 <jesus87fdez@gmail.com>
2023-10-21 19:21:13 -0700

    Primera-Version Spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Navbar from "@components/NavBar";
+import Spinner from "@components/Spinner";
 import TodoList from "@components/TodoList";
 import LoginScreen from "@screens/LoginScreen";
 
@@ -6,7 +8,8 @@ function App() {
     <div className="App">
       {/* <TodoList title="asasd" number={12313} color={"green"} /> */}
 
-      <LoginScreen />
+      {/**<LoginScreen /> */}
+      <Spinner/>
     </div>
   );
 }
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,13 @@
+
+function Spinner() {
+  return (
+    <>
+      <div className="spinnerContainer">
+        <div className="spinner"></div>
+        <strong>Cargando</strong>
+      </div>
+    </>
+  );
+}
+
+export default Spinner;
