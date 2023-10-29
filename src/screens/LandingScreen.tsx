8c9f2f687674ec375100f7476a0796dac8e44269
Sebastian <wiserquill@gmail.com>
2023-10-28 18:23:06 -0700

    Estructura baja del Landing Page

--- src/screens/LandingScreen.tsx
+++ src/screens/LandingScreen.tsx
@@ -21,12 +21,31 @@ const LandingScreen = (props: LandingScreenProps) => {
             <section className="landing-welcome"></section>
             <section className="landing-info">
                 <div className="landing-info-content">
-                    <div className="landing-info-card"></div>
-                    <div className="landing-info-card"></div>
-                    <div className="landing-info-card"></div>
+                    <div className="landing-info-card"> 
+                      <img src={iconoTask} className='footer-img'></img>  
+                      <p>
+                        Usada para calificar contenido, más estrellas indican mejor calidad o rendimiento del elemento evaluado.
+                      </p>
+                    </div>
+                    <div className="landing-info-card">
+                      <img src={iconoCalendario} className='footer-img'></img>  
+                      <p>
+                        Muestra una hora específica para una fecha límite, indicando el plazo para completar una tarea importante.
+                      </p>
+                    </div>
+                    <div className="landing-info-card">
+                      <img src={iconoHouse} className='footer-img'></img>  
+                      <p>
+                        Representa la opción de realizar tareas escolares desde casa, brindando comodidad y flexibilidad en el entorno educativo.
+                      </p>
+                    </div>
                 </div>
             </section>
-            <footer className="landing-footer"></footer>
+            <footer className="landing-footer">
+              <img src={logo} className='footer-img'></img>
+              <p>Acerca de StudySync</p>
+              <p>Copyright ©2023 StudySync</p>
+            </footer>
         </div>
     );
 };
