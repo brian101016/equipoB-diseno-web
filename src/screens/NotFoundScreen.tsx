e38f58751466f7a34b9d4bd27639216e1e053e5d
Oscar Martinez de Castro <you@example.com>
2023-11-03 10:59:09 -0700

    avances componente lista tarea

--- src/screens/NotFoundScreen.tsx
+++ src/screens/NotFoundScreen.tsx
@@ -1,37 +1,86 @@
 // ################################ INTERFACES & PROPS ################################
 
-import { useRouteError } from "react-router-dom";
+import { useRouteError } from 'react-router-dom';
+import RowList from '@components/RowList';
 
 // NotFoundScreen => Rename all instances to use
 type NotFoundScreenProps = {};
 
 // ################################ RENDERING COMPONENT ################################
 const NotFoundScreen = (props: NotFoundScreenProps) => {
-  const err = useRouteError() as any;
+    const err = useRouteError() as any;
 
-  const info = err
-    ? {
-        status: err.status || "inesperado",
-        reason: err.data || "Algo salió mal...",
-        message:
-          err.statusText || err.message || "Vuelva a intentarlo más tarde.",
-      }
-    : {};
+    const info = err
+        ? {
+              status: err.status || 'inesperado',
+              reason: err.data || 'Algo salió mal...',
+              message:
+                  err.statusText ||
+                  err.message ||
+                  'Vuelva a intentarlo más tarde.',
+          }
+        : {};
 
-  // ------------------------------------------------------------------------------------ RETURN
-  return (
-    <div>
-      {err ? (
-        <>
-          <h1>Error {info.status}</h1>
-          <h2>{info.reason}</h2>
-          <h4>{info.message}</h4>
-        </>
-      ) : (
-        <>Página no encontrada</>
-      )}
-    </div>
-  );
+    // ------------------------------------------------------------------------------------ RETURN
+    return (
+        <div>
+            {err ? (
+                <>
+                    <h1>Error {info.status}</h1>
+                    <h2>{info.reason}</h2>
+                    <h4>{info.message}</h4>
+                </>
+            ) : (
+                // Pruebas temporales [Eliminar todo el codigo de prueba aqui]
+                <>
+                    Página no encontrada
+                    <h1>Lista de Tareas</h1>
+                    <RowList
+                        svgStatus={0}
+                        numActivity={1}
+                        titulo={'Tarea de prueba'}
+                        fecha="10/11/2023"
+                        hora="23:59:00"
+                        calificacion={85}
+                    />
+                    <RowList
+                        svgStatus={1}
+                        numActivity={2}
+                        titulo={'Resumen libro del gabacho'}
+                        fecha="12/11/2023"
+                        hora="23:59:00"
+                        calificacion={50}
+                    />
+                    <RowList
+                        svgStatus={3}
+                        numActivity={2}
+                        titulo={'Practica codigo HTML'}
+                        fecha="29/10/2023"
+                        hora="23:59:00"
+                        calificacion={100}
+                    />
+                    <RowList
+                        svgStatus={3}
+                        numActivity={2}
+                        titulo={'Practica codigo HTML'}
+                        fecha="29/10/2023"
+                        hora="23:59:00"
+                        calificacion={0}
+                    />
+                    <RowList
+                        svgStatus={3}
+                        numActivity={2}
+                        titulo={'Practica codigo HTML'}
+                        fecha="03/11/2023"
+                        hora="23:59:00"
+                    />
+                    <h2>termina la lista de tareas</h2>
+                </>
+
+                // Aqui termina el codigo de prueba
+            )}
+        </div>
+    );
 };
 
 // ################################ EXPORTS ################################
